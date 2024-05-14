bf0a1e3e28438c21d7453f2cada0eefe5fb292cc
Branislav Juhás <juhas.branislaw@gmail.com>
2024-05-14 10:12:20 +0200

    Router Animations Added 140524

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 </script>
@@ -7,28 +6,50 @@ import Footer from "./components/Footer.vue";
 <template>
   <div id="view">
     <Header />
-      <div id="content">
-        <div class="centered">
-          <router-view />
-        </div>
+    <div id="content">
+      <div class="centered">
+        <router-view id="router" v-slot="{ Component }">
+          <Transition name="slide" mode="out-in">
+            <component :is="Component" />
+          </Transition>
+        </router-view>
       </div>
+    </div>
     <Footer />
   </div>
 </template>
 
 <style scoped>
-  #view {
-    display: grid;
-    grid-template-rows: 3.75rem 1fr 3.75rem;
-    grid-template-columns: 100%;
-    height: 100%;
-    width: 100%;
-  }
+#view {
+  display: grid;
+  grid-template-rows: 3.75rem 1fr 3.75rem;
+  grid-template-columns: 100%;
+  height: 100%;
+  width: 100%;
+}
 
-  #content {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-  }
+#content {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition:
+    transform 0.21s,
+    opacity 0.21s ease;
+}
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(30%);
+  opacity: 0;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateY(0);
+  opacity: 1;
+}
 </style>
