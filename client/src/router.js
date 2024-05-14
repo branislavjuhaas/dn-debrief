77bb176269a16970c283b4af5a0878d38d8ca112
Branislav Juhás <juhas.branislaw@gmail.com>
2024-05-14 10:12:46 +0200

    Routes Updates 140524

--- client/src/router.js
+++ client/src/router.js
@@ -1,24 +1,42 @@
-import { createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import Error from "./views/Error.vue";
+import Auth from "./views/Auth.vue";
+import Create from "./views/Create.vue";
+import User from "./views/User.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        name: "NotFound",
-        component: Error,
-        props: { code: 404 },
-    }
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/users/auth",
+    name: "Auth",
+    component: Auth,
+  },
+  {
+    path: "/users/create",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/users/:id",
+    name: "User",
+    component: User,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: Error,
+    props: { code: 404 },
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
---
+++ client/src/views/Create.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Create</h1>
+  </div>
+</template>
+
+<style scoped></style>
