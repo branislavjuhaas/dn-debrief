b0d33ba0c0c9b1879666ce52c9db60b67da63375
Branislav Juhás <juhas.branislaw@gmail.com>
2024-07-30 23:17:58 +0200

    Error Creation 300724

---
+++ src/views/Error.vue
@@ -0,0 +1,30 @@
+<script setup>
+// get the error from the props
+const props = defineProps(["code"]);
+
+// set the error message based on the error code
+let message;
+let description;
+switch (props.code) {
+  case 404:
+    message = "Stránka nenájdená";
+    description = "Stránka, ktorú sa snažíte zobraziť, neexistuje.";
+    break;
+  case 500:
+    message = "Serverová chyba";
+    break;
+  default:
+    message = "Nastala chyba";
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-5xl">
+      <b>{{ code + " - " + message }}</b>
+    </h1>
+    <p class="text-xl mt-2">{{ description }}</p>
+  </div>
+</template>
+
+<style scoped></style>
