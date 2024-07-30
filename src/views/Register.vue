3bf2a61a5563b8ac0aca9e30404deddbc960f3ec
Branislav Juhás <juhas.branislaw@gmail.com>
2024-07-30 23:18:08 +0200

    Register Creation 300724

---
+++ src/views/Register.vue
@@ -0,0 +1,82 @@
+<script setup>
+import Field from "../components/Field.vue";
+import Toggle from "../components/Toggle.vue";
+import { ref, watch } from "vue";
+
+const email = ref("");
+const name = ref("");
+const surname = ref("");
+const password = ref("");
+const confirm = ref("");
+
+const message = ref("");
+const canSubmit = ref(false);
+
+// Watch the password and confirm fields
+watch([password, confirm], ([password, confirm]) => {
+  if (password !== confirm) {
+    message.value = "Heslá sa nezhodujú";
+  } else {
+    message.value = "";
+  }
+});
+
+// Watch to update the value of the enabled submit button
+watch([email, name, surname, password, confirm, message], () => {
+  canSubmit.value =
+    email.value !== "" &&
+    name.value !== "" &&
+    surname.value !== "" &&
+    password.value !== "" &&
+    confirm.value !== "" &&
+    message.value === "";
+});
+</script>
+
+<template>
+  <div class="gap-4">
+    <h1 class="text-5xl font-bold mb-2">Registrácia do systému</h1>
+    <div
+      class="flex flex-col justify-between w-full bg-white min-h-60 rounded-[1.25rem] p-5 gap-16">
+      <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <field
+          name="username"
+          v-model="email"
+          label="Email"
+          type="text"
+          class="col-span-1 sm:col-span-2" />
+        <field v-model="name" label="Meno" type="text" />
+        <field v-model="surname" label="Priezvisko" type="text" />
+        <field
+          name="password"
+          v-model="password"
+          label="Heslo"
+          type="password" />
+        <field v-model="confirm" label="Potvrdenie hesla" type="password" />
+      </div>
+      <div
+        class="grid grid-flow-col gap-4 items-center sm:grid-rows-1 sm:grid-cols-[1fr_auto]">
+        <p
+          v-if="message !== ''"
+          class="form-message row-start-3 sm:row-start-1">
+          {{ message }}
+        </p>
+        s
+        <div
+          :class="canSubmit ? 'form-primary' : 'form-primary-disabled'"
+          class="vertical-center col-start-1 sm:col-start-3">
+          <p>Vytvoriť účet</p>
+        </div>
+      </div>
+    </div>
+    <router-link to="/auth" class="alternative vertical-center w-full">
+      <p>Prihlásiť sa s existujúcim účtom</p>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+.alternative {
+  @apply flex flex-row items-center h-12 bg-white text-black rounded-[1.25rem] border-2 border-red border-opacity-0 font-bold px-5 duration-150 cursor-pointer hover:border-opacity-100;
+}
+</style>
