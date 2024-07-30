fa2c32e33e819577e517d85b7ff4ed758d27df87
Branislav Juhás <juhas.branislaw@gmail.com>
2024-07-30 23:18:40 +0200

    Pinia User Store Creation 300724

---
+++ src/stores.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore("user", {
+  state: () => ({
+    uid: null,
+    name: null,
+    surname: null,
+    email: null,
+    role: null,
+  }),
+  getters: {
+    fullName() {
+      // if uid null return null
+      return this.uid ? `${this.name} ${this.surname}` : null;
+    },
+  },
+  actions: {
+    setUser(uid, email, name, surname, role) {
+      this.uid = uid;
+      this.email = email;
+      this.name = name;
+      this.surname = surname;
+      this.role = role;
+    },
+    logOut() {
+      this.uid = null;
+      this.name = null;
+      this.surname = null;
+      this.email = null;
+      this.role = null;
+    },
+  },
+});
