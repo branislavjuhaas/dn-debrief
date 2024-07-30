650cbcc466169d91d376b15cbd26b3b482f06a4c
Branislav Juhás <juhas.branislaw@gmail.com>
2024-07-30 23:16:40 +0200

    Dependencies Installation 300724

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,css,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      epilogue: ["Epilogue", "sans-serif"],
+    },
+    colors: {
+      green: "#21d46e",
+      black: "#000000",
+      white: "#ffffff",
+      red: "#e81525",
+      grey: "#888888",
+    },
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
