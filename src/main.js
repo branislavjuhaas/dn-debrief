f885b1c505d666533f60136793a61256e4249222
Branislav Juhás <juhas.branislaw@gmail.com>
2024-07-31 10:29:06 +0200

    Firebase Credentials Addition 300724

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCG1YinvyCiYK2ppM6lNDoO1Jw8PXYToDE",
+  authDomain: "dn-cascade.firebaseapp.com",
+  databaseURL:
+    "https://dn-cascade-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dn-cascade",
+  storageBucket: "dn-cascade.appspot.com",
+  messagingSenderId: "302805903197",
+  appId: "1:302805903197:web:96d3607464b5d0ce38eab4",
+  measurementId: "G-8WDZKR0VPT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 const pinia = createPinia();
 
