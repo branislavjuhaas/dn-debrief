39e1d38d229dcf3d7405dd11c911a5ba2e07b43c
Branislav Juhás <juhas.branislaw@gmail.com>
2024-08-03 11:03:37 +0200

    Privacy Policy Addition 030824

---
+++ src/views/Privacy.vue
@@ -0,0 +1,92 @@
+<script setup></script>
+
+<template>
+  <div class="text-justify">
+    <h1 class="text-5xl font-bold mb-2">
+      Vyhlásenie Slovenskej debatnej asociácie o ochrane osobných údajov pre
+      službu DebRIEF
+    </h1>
+    <h3>Aké osobné údaje spracúvame a ako ich získavame?</h3>
+    <p>
+      Rozsah dát, ktoré spracúvame, závisí od kontextu Vašej interakcie so
+      službou DebRIEF. Od používateľov, ktorí službu používajú ako hostia bez
+      účtu, zbierame len adresu IP. Od Používateľov, ktorí službu používajú s
+      bežným účtom bez registrácie v SDA, zbierame adresu IP, emailovú adresu,
+      meno a priezvisko spolu s uživateľským agentom prehliadača. Od
+      Používateľov, ktorí službu používajú s účtom registrovaným v SDA, zbierame
+      adresu IP, emailovú adresu, meno, priezvisko, uživateľského agenta.
+      <br />
+      <br />
+      Osobné údaje získavame priamo od Vás, Vašich zástupcov alebo pri Vašej
+      interakcii so službou DebRIEF. Údaje používame na identifikáciu a kontakt
+      s Vami, na zabezpečenie bezpečnosti služby a na jej zlepšenie.
+    </p>
+    <h3>Aký je právny základ spracúvania Vašich osobných údajov?</h3>
+    <p>
+      Vaše údaje potrebujeme na základe našich oprávnených záujmov v súvislosti
+      s plnením našich právnych povinností súvisiacich s ochranou zdravia a
+      bezpečnosti účastníkov a účastníčok podujatia.
+    </p>
+    <h3>Komu poskytujeme Vaše osobné údaje?</h3>
+    <p>
+      Vaše osobné údaje uvedené v tomto dokumente poskytneme iba osobám
+      poskytujúcim prvú pomoc alebo zdravotnú starostlivosť.
+    </p>
+    <h3>Ako chránime Vaše osobné údaje?</h3>
+    <p>
+      Tento dokument zničíme najneskôr mesiac po ukončení podujatia.
+      Podrobnejšie informácie o ochrane osobných údajov v našej organizácii
+      nájdete na www.sda.sk/sukromie. Ak máte ďalšie otázky ohľadne ochrany
+      osobných údajov, prosím kontaktujte nás na adrese sda@sda.sk.
+    </p>
+    <h3>Aké sú Vaše práva?</h3>
+    <div>
+      <p>V súvislosti so spracúvaním osobných údajov máte:</p>
+      <ul class="list-disc list-inside">
+        <li>
+          právo na jasné, transparentné a zrozumiteľné informácie o tom, ako
+          používame Vaše osobné údaje a aké sú Vaše práva (na to slúži aj tento
+          text ako aj informácie publikované na www.sda.sk/sukromie);
+        </li>
+        <li>
+          právo na prístup k Vašim osobným údajom a poskytnutie ďalších
+          informácií súvisiacich s ich spracúvaním zo strany našej organizácie;
+          - právo na opravu nesprávnych a neúplných osobných údajov;
+        </li>
+        <li>
+          právo na vymazanie Vašich osobných údajov predovšetkým a) ak už nie sú
+          ďalej potrebné pre ďalšie spracúvanie; b) ste odvolali súhlas na ich
+          spracovanie; c) sa spracúvali nezákonne; d) ste oprávnene namietali
+          voči ich spracúvaniu; alebo e) musia byť vymazané v zmysle právnych
+          predpisov.
+        </li>
+        <li>
+          právo na obmedzenie spracúvania Vašich osobných údajov, ak a)
+          napadnete správnosť osobných údajov po dobu, kým overíme ich
+          správnosť; b) spracúvanie je protizákonné; alebo ak c) namietate proti
+          ich spracúvaniu po dobu, kým overíme, či naše oprávnené dôvody
+          prevažujú nad Vašimi záujmami;
+        </li>
+        <li>
+          právo podať námietky proti spracúvaniu Vašich osobných údajov v
+          prípade, že ich spracúvame na účely našich oprávnených záujmov;
+        </li>
+        <li>
+          právo získať svoje osobné údaje a preniesť ich k inému poskytovateľovi
+          služieb;
+        </li>
+        <li>
+          právo podať sťažnosť na Úrade na ochranu osobných údajov, so sídlom
+          Hraničná 12, 820 07 Bratislava, tel. č. +421 2 3231 3220, e-mail
+          statny.dozor@pdg.gov.sk.
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  @apply text-2xl font-bold mt-4;
+}
+</style>
