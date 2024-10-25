a06451022988372b9cdd9b2f10b6d104440b29c4
Branislav Juhás <juhas.branislaw@gmail.com>
2024-10-25 20:14:28 +0200

    Async Firestore Caching - #15 251024

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getFunctions } from "firebase/functions";
 import vue3GoogleLogin from "vue3-google-login";
 import { initializeApp } from "firebase/app";
-import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 import { createPinia } from "pinia";
 import router from "./router.js";
 import { createApp } from "vue";
@@ -54,7 +53,24 @@ export const functions = getFunctions(app);
 // Get Firebase Analytics instance
 export const analytics = getAnalytics(app);
 
-initializeFirestore(app, { localCache: persistentLocalCache({}) });
+/**
+ * Initializes Firestore with a persistent local cache.
+ *
+ * This function dynamically imports the necessary Firestore functions and initializes Firestore
+ * with a persistent local cache.
+ *
+ * @async
+ * @function firestoreInitialize
+ * @returns {Promise<void>}
+ */
+const initCaching = async () => {
+  const { initializeFirestore, persistentLocalCache } = await import(
+    "firebase/firestore"
+  );
+
+  // Initialize Firestore with a persistent local cache
+  initializeFirestore(app, { localCache: persistentLocalCache({}) });
+};
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
@@ -65,3 +81,5 @@ export const appCheck = initializeAppCheck(app, {
   // tokens as needed.
   isTokenAutoRefreshEnabled: true,
 });
+
+initCaching();
