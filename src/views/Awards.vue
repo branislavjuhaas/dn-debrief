5210d1ddeac1184587cf3b99147a6aaf3feea425
Branislav Juhás <juhas.branislaw@gmail.com>
2024-11-07 16:36:34 +0100

    Awards Exploration Page Update 071124

--- src/views/Awards.vue
+++ src/views/Awards.vue
@@ -1,80 +1,109 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { getAllAwards } from "../firebase/awards.js";
 import { useUserStore } from "../stores.js";
 
 // State variables
 const awards = ref([]);
-const selectedAward = ref(null);
 const userStore = useUserStore();
 
-// Function to fetch all awards
+/**
+ * Fetch all awards and filter them based on user role and ownership.
+ * If the user is a developer, display all awards.
+ * Otherwise, display awards with category 'program' or 'organization' or those owned by the user.
+ */
 const fetchAwards = async () => {
   awards.value = await getAllAwards();
-  selectRandomAward();
-};
 
-// Function to select a random award based on the date
-const selectRandomAward = () => {
-  const today = new Date().getDate();
-  selectedAward.value = awards.value[today % awards.value.length];
-};
-
-// Function to handle award selection
-const handleAwardSelection = (award) => {
-  selectedAward.value = award;
-};
-
-// Function to handle scroll event and change selected award
-const handleScroll = (event) => {
-  const scrollAmount = event.deltaY;
-  if (scrollAmount > 0) {
-    const currentIndex = awards.value.indexOf(selectedAward.value);
-    const nextIndex = (currentIndex + 1) % awards.value.length;
-    selectedAward.value = awards.value[nextIndex];
-  } else {
-    const currentIndex = awards.value.indexOf(selectedAward.value);
-    const prevIndex = (currentIndex - 1 + awards.value.length) % awards.value.length;
-    selectedAward.value = awards.value[prevIndex];
+  if (userStore.role !== "developer") {
+    awards.value = awards.value.filter(
+      (award) =>
+        award.category === "program" ||
+        award.category === "organization" ||
+        userStore.awards.some((userAward) => userAward.id === award.id),
+    );
   }
+
+  // Update the symbol's background color based on award ownership
+  awards.value.forEach((award) => {
+    if (
+      userStore.awards.legend.some((userAward) => userAward.id === award.id)
+    ) {
+      award.symbol = `
+        <defs>
+          <linearGradient id='legend-gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
+            <stop offset='0%' style='stop-color:#ffd900;stop-opacity:1' />
+            <stop offset='100%' style='stop-color:#c3a601;stop-opacity:1' />
+          </linearGradient>
+        </defs>
+        <mask id='legend-mask'>${award.symbol}</mask>
+        <rect width='20' height='20' fill='url(#legend-gradient)' mask='url(#legend-mask)' />
+      `;
+      award.symbol = award.symbol.replaceAll("black", "white");
+    }
+  });
 };
 
+// Fetch awards on component mount or when user role is set
 onMounted(() => {
-  fetchAwards();
+  if (userStore.role) {
+    fetchAwards();
+  } else {
+    const unwatch = watch(
+      () => userStore.role,
+      (newRole) => {
+        if (newRole) {
+          fetchAwards();
+          unwatch();
+        }
+      },
+    );
+  }
 });
 </script>
 
 <template>
   <div class="gap-4">
     <h1 class="text-5xl font-bold mb-2">Ocenenia</h1>
-    <div class="flex flex-col w-full text-black bg-white min-h-60 rounded-[1.25rem] p-5 gap-8 transition-all">
-      <div class="flex flex-col gap-4">
-        <div class="grid grid-rows-1 font-bold gap-4 items-center grid-cols-3">
-          <p>Award Name</p>
-          <p>Description</p>
-          <p>Visual List</p>
-        </div>
-      </div>
-      <div class="flex flex-col gap-4">
-        <div class="grid grid-cols-3 items-center cursor-pointer gap-4 rounded-[1.25rem] duration-150 transition-all delay-300 hover:py-5 hover:text-red">
-          <p class="truncate">{{ selectedAward?.name }}</p>
-          <p class="overflow-hidden sm:truncate">{{ selectedAward?.description }}</p>
-          <div class="flex flex-row">
-            <div v-for="award in awards" :key="award.id" @click="handleAwardSelection(award)" class="flex flex-row relative items-center justify-center w-20">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" viewBox="0 0 20 20" fill="currentColor" v-html="award.symbol"></svg>
-              <div v-if="award !== selectedAward" class="absolute right-0 w-[2px] h-8 bg-black rounded-full"></div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div
+      class="flex flex-row flex-wrap justify-start shrink-0 items-center w-full text-black bg-white min-h-60 rounded-[1.25rem] p-5 gap-8 transition-all">
+      <router-link
+        v-for="award in awards"
+        :key="award.id"
+        :to="'/awards/' + award.id"
+        class="flex flex-col flex-grow flex-basis-1/3 w-1/5 h-32 rounded-[1.25rem] justify-center items-center gap-2 m-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-20 w-20"
+          viewBox="0 0 20 20"
+          v-html="award.symbol" />
+        <p
+          class="font-bold text-center text-pretty"
+          :class="{
+            'selected-award': !(
+              userStore.awards.ordinary.some(
+                (userAward) => userAward.id === award.id,
+              ) ||
+              userStore.awards.legend.some(
+                (userAward) => userAward.id === award.id,
+              )
+            ),
+          }">
+          {{
+            userStore.awards.legend.some(
+              (userAward) => userAward.id === award.id,
+            )
+              ? award.legendary.title
+              : award.regular.title
+          }}
+        </p>
+      </router-link>
     </div>
   </div>
 </template>
 
 <style scoped>
 .selected-award {
-  transform: scale(1.2);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  background-color: blue;
+  @apply font-normal;
 }
 </style>
