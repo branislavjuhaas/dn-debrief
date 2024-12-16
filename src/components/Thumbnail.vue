4833862c8c9a1166c68bea5b7bfec02b4063eaee
Branislav Juhás <juhas.branislaw@gmail.com>
2024-12-16 20:55:35 +0100

    Duration Added 161224

--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -160,7 +160,7 @@ const datesAggregate = () => {
       </p>
     </div>
     <div
-      class="thumbnail-controls flex absolute bottom-0 h-14 p-2 w-full opacity-0 items-centers gap-4">
+      class="thumbnail-controls flex absolute bottom-0 h-14 p-2 w-full opacity-0 items-centers gap-4 duration-300">
       <button
         @click="$refs.fileInput.click()"
         class="flex w-full bg-red h-10 items-center px-4 vertical-center rounded-[0.75rem] border-2 border-black justify-center hover:bg-black duration-300">
