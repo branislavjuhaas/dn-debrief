5d765949cfe93ffdfce67eb32b997ea5e8ddd2de
Branislav Juhás <juhas.branislaw@gmail.com>
2024-12-24 23:16:19 +0100

    Header Logo Width Fix 241224

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -33,7 +33,7 @@ else if (window.location.hostname === "barca.juhaas.eu") {
   <div class="flex flex-col h-[3.75rem] items-center px-5">
     <div
       class="grid grid-rows-1 grid-cols-[auto_1fr] gap-4 justify-center items-center max-w-[1320px] h-full w-full sm:grid-cols-[1fr_31.25rem_1fr]">
-      <router-link to="/" class="flex items-center">
+      <router-link to="/" class="flex w-fit items-center">
         <img :src="logo" :alt="system" class="h-8" />
       </router-link>
       <Search />
