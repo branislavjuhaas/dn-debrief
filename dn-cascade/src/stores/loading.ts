a2f49a2f0b812d92ddad51066e180cf2aedba773
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-09 21:04:57 +0200

    feat: add loading and user stores for state management

---
+++ dn-cascade/src/stores/loading.ts
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+
+export const useLoadingStore = defineStore("loading", {
+  state: () => ({
+    loadings: 0,
+  }),
+  getters: {
+    loading() {
+      return this.loadings > 0;
+    },
+  },
+  actions: {
+    loadingStart() {
+      this.loadings++;
+    },
+    loadingEnd() {
+      this.loadings--;
+    },
+  },
+});
