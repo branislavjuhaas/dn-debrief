80499af644f5fe8423e3de6e1091e25222540d73
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-11 09:29:48 +0200

    fix: remove outdated comment regarding Firebase SDK measurementId

--- dn-cascade/src/main.ts
+++ dn-cascade/src/main.ts
@@ -9,7 +9,6 @@ import { initializeApp } from "firebase/app";
 const pinia = createPinia();
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCG1YinvyCiYK2ppM6lNDoO1Jw8PXYToDE",
   authDomain: "dn-cascade.firebaseapp.com",
