285b3786eaac3e88adff6d553dc59f251b0f0678
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-27 14:54:34 +0200

    fix: add gap to layout component for improved spacing between elements

--- dn-cascade/layouts/default.vue
+++ dn-cascade/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col w-full px-5 items-center text-white">
-    <div class="flex flex-col max-w-content w-full pt-18">
+    <div class="flex flex-col max-w-content w-full pt-18 gap-16">
       <slot />
     </div>
   </div>
