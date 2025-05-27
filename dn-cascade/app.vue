cf8312dae3b8981ae81e0b0672cc9080f58dcb9f
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-27 11:58:47 +0200

    feat: restructure app layout with header and footer for improved UX

--- dn-cascade/app.vue
+++ dn-cascade/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <nuxt-page />
+  <div class="flex flex-col h-dvh w-full">
+    <app-header class="fixed w-full top-0 z-50" />
+    <main class="flex-1 pt-[3.75rem] pb-[3.75rem]">
+      <nuxt-layout>
+        <nuxt-page />
+      </nuxt-layout>
+    </main>
+    <app-footer class="fixed bottom-0 w-full z-50" />
   </div>
 </template>
 
