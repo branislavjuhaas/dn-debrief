7ed19122979f6538f8b59dfae1df24a111c9505b
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-27 11:56:36 +0200

    feat: add footer and button components with responsive design

---
+++ dn-cascade/components/app/footer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex flex-col bg-white overflow-hidden min-h-[3.75rem] items-center justify-center px-5 border-t-2 border-black">
+    <cookies-banner v-if="false" />
+    <div
+      v-else
+      class="flex flex-row justify-between items-center max-w-[1320px] h-full w-full">
+      <p class="hidden mt-1 sm:flex">
+        2024 - 2025&nbsp;&nbsp;&nbsp;Branislav Juhás
+      </p>
+
+      <div class="flex flex-row gap-14 items-center w-full sm:w-auto">
+        <nuxt-link to="/privacy" class="hidden mt-1 sm:flex">GDPR</nuxt-link>
+        <!--          v-if="user.role && user.role !== 'user'"-->
+        <nuxt-link to="/manage" class="hidden mt-1 sm:flex">
+          {{ "Vývojár - Správa" }}
+        </nuxt-link>
+        <app-footer-button link="/auth" class="w-full sm:w-auto">
+          {{ "Prihlásiť sa" }}
+        </app-footer-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ dn-cascade/components/app/footer/button.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const props = defineProps({
+  link: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+
+/**
+ * This function handles the click event for the button.
+ * It emits a click event if the button is not disabled.
+ * @param event - The click event.
+ */
+const handle = (event: MouseEvent) => {
+  emit("click");
+};
+</script>
+
+<template>
+  <button v-if="!props.link" class="button" @click="handle">
+    <span>
+      <slot />
+    </span>
+  </button>
+  <nuxt-link v-else :to="props.link" class="button">
+    <span>
+      <slot />
+    </span>
+  </nuxt-link>
+</template>
+
+<style scoped>
+@reference "~/assets/css/main.css";
+
+.button {
+  @apply flex min-w-40 px-5 h-10 border-2 border-black rounded-2xl items-center justify-center hover:bg-red hover:!text-white;
+}
+
+.button > span {
+  @apply mt-1;
+}
+</style>
