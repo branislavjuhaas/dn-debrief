df65c3bb9699f1ef1a227c658d15d49cdbe67f8d
Branislav Juhás <juhas.branislaw@gmail.com>
2025-05-27 12:03:44 +0200

    feat: add default layout component with responsive design

---
+++ dn-cascade/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col w-full px-5 items-center">
+    <div class="flex flex-col max-w-content w-full pt-18">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
