8a4068aae88583b5664d0acbbe4e72b30c0cc8f6
Branislav Juhás <juhas.branislaw@gmail.com>
2025-06-01 13:58:23 +0200

    feat: add @nuxtjs/supabase module to configuration

--- dn-cascade/nuxt.config.ts
+++ dn-cascade/nuxt.config.ts
@@ -3,7 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/google-fonts", "@pinia/nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/google-fonts",
+    "@pinia/nuxt",
+    "@nuxtjs/supabase",
+  ],
   devServer: {
     port: 7210,
   },
@@ -15,6 +20,9 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
+  supabase: {
+    redirect: false,
+  },
   googleFonts: {
     preload: true,
     families: {
