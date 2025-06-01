56c31bf1c43e60c58903d0c0953e59050a21946b
Branislav Juhás <juhas.branislaw@gmail.com>
2025-06-01 13:49:02 +0200

    refactor: remove unused Firebase dependencies and vuefire module from configuration

--- dn-cascade/nuxt.config.ts
+++ dn-cascade/nuxt.config.ts
@@ -3,12 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "nuxt-vuefire",
-    "@nuxtjs/google-fonts",
-    "@pinia/nuxt",
-  ],
+  modules: ["@nuxt/eslint", "@nuxtjs/google-fonts", "@pinia/nuxt"],
   devServer: {
     port: 7210,
   },
@@ -20,28 +15,6 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
-  vuefire: {
-    auth: {
-      enabled: true,
-      sessionCookie: true,
-    },
-    appCheck: {
-      isTokenAutoRefreshEnabled: true,
-      provider: "ReCaptchaV3",
-      key: "6Letq2oqAAAAAChut_Xa1Xg9AHu_n76VKWt9cTlo",
-    },
-    config: {
-      apiKey: "AIzaSyCG1YinvyCiYK2ppM6lNDoO1Jw8PXYToDE",
-      authDomain: "dn-cascade.firebaseapp.com",
-      databaseURL:
-        "https://dn-cascade-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "dn-cascade",
-      storageBucket: "dn-cascade.appspot.com",
-      messagingSenderId: "302805903197",
-      appId: "1:302805903197:web:96d3607464b5d0ce38eab4",
-      measurementId: "G-8WDZKR0VPT",
-    },
-  },
   googleFonts: {
     preload: true,
     families: {
