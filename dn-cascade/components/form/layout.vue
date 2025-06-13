080d464250b74d2eb4bd8b38a8d07d33edab450c
Branislav Juhás <juhas.branislaw@gmail.com>
2025-06-13 17:13:59 +0200

    feat: add yellow color variable to theme

--- dn-cascade/components/form/layout.vue
+++ dn-cascade/components/form/layout.vue
@@ -1,16 +1,49 @@
 <template>
-  <app-wrapper class="grid grid-cols-2">
+  <app-wrapper class="md:grid md:grid-cols-2">
     <div
       class="flex flex-col min-h-full items-center justify-between gap-16 p-5">
       <slot />
     </div>
     <div
-      class="bg-[url('/images/dn-banner.png')] bg-center bg-no-repeat w-full h-full flex items-center justify-center">
-      <img src="@/assets/images/form/dn-logo.svg" alt="DN" />
+      class="max-md:hidden bg-[url('/images/dn-banner.png')] bg-center bg-no-repeat w-full h-full flex items-end justify-center relative">
+      <img
+        src="@/assets/images/form/dn-logo.svg"
+        alt="DN"
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10" />
+      <Transition name="fly-in" mode="out-in">
+        <form-warning v-if="warnings.length > 0" :warning="warnings[0]" />
+      </Transition>
     </div>
   </app-wrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const warnings = ref<string[]>([]);
 
-<style scoped></style>
+const warn = (message: string): void => {
+  warnings.value.push(message);
+  setTimeout(() => {
+    const idx = warnings.value.indexOf(message);
+    if (idx !== -1) warnings.value.splice(idx, 1);
+  }, 5000);
+};
+
+defineExpose({
+  warn,
+});
+</script>
+
+<style scoped>
+.fly-in-enter-active,
+.fly-in-leave-active {
+  transition: all 0.3s ease;
+}
+.fly-in-enter-from,
+.fly-in-leave-to {
+  transform: translateY(100%);
+}
+.fly-in-enter-to,
+.fly-in-leave-from {
+  transform: translateY(0);
+}
+</style>
---
+++ dn-cascade/components/form/warning.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="grid grid-cols-[1fr_1.25rem] gap-2 w-full p-5 bg-yellow font-bold border-t-2 border-black z-20 items-center relative">
+    <hr
+      class="loader h-[3px] bg-red border-0 absolute top-0 left-0 rounded-br-[2px]" />
+    <p class="mt-1">{{ props.warning }}</p>
+    <Icon name="dn:warning-f" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  warning: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.loader {
+  width: 100%;
+  animation: load 5s linear forwards;
+}
+
+@keyframes load {
+  0% {
+    width: 100%;
+  }
+  100% {
+    width: 0%;
+  }
+}
+</style>
