76313dd2d9d424f16a52a9425effd188626dd13e
Branislav Juhás <juhas.branislaw@gmail.com>
2025-06-13 16:21:15 +0200

    feat: create wrapper component with template and styles

---
+++ dn-cascade/components/app/wrapper.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full min-h-64 bg-white text-black rounded-2xl overflow-hidden">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
