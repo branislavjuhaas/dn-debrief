677f7c9395cb1225d0863ad5b52e88ee7bb99f6d
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-06 19:53:29 +0200

    feat: add footer component and update header text color

---
+++ dn-debrief/components/app/footer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="fixed bottom-0 w-full flex flex-col bg-white text-black min-h-[3.75rem] items-center justify-center px-5 border-t-2 border-black"
+  >
+    <!-- <cookies-banner v-if="false" /> -->
+    <div
+      class="flex flex-row justify-between items-center max-w-[1320px] h-full w-full"
+    >
+      <p class="hidden mt-1 sm:flex">
+        2024 - 2025&nbsp;&nbsp;&nbsp;&nbsp;Branislav Juhás
+      </p>
+
+      <div class="flex flex-row gap-14 items-center w-full sm:w-auto">
+        <nuxt-link to="/privacy" class="hidden mt-1 sm:flex">GDPR</nuxt-link>
+        <!--          v-if="user.role && user.role !== 'user'"-->
+        <nuxt-link to="/events" class="hidden mt-1 sm:flex">
+          Podujatia
+        </nuxt-link>
+        <!-- <div
+          v-if="userStore.isAuthenticated"
+          class="w-full sm:w-auto flex flex-row gap-1 items-center"
+        >
+          <app-footer-button link="/me" class="w-full" type="left-alone">
+            {{ userStore.fullName || "Profil" }}
+          </app-footer-button>
+          <app-footer-menu />
+        </div>
+        <app-footer-button v-else link="/auth" class="w-full sm:w-auto">
+          Prihlásiť sa
+        </app-footer-button> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- dn-debrief/components/app/header.vue
+++ dn-debrief/components/app/header.vue
@@ -4,7 +4,7 @@ import { NuxtImg } from "#components";
 
 <template>
   <header
-    class="fixed top-0 w-full bg-white flex flex-col h-[3.75rem] items-center px-5 border-b-2 border-black"
+    class="fixed top-0 w-full bg-white text-black flex flex-col h-[3.75rem] items-center px-5 border-b-2 border-black"
   >
     <div
       class="grid grid-cols-[auto_1fr] sm:grid-cols-[1fr_auto_1fr] max-w-content h-full w-full gap-4 justify-center items-center"
