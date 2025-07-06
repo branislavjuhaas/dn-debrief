eaa7921d2025baabf5258e78f55e9cd31d47fc8d
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-06 23:09:03 +0200

    feat: enhance dropdown and drawer components for improved navigation and user experience

--- dn-debrief/components/app/footer/menu.vue
+++ dn-debrief/components/app/footer/menu.vue
@@ -1,46 +1,92 @@
 <template>
-  <DropdownMenuRoot v-model:open="toggleState" class="w-fit" :modal="false">
-    <DropdownMenuTrigger
-      class="flex flex-row px-5 border-2 h-10 items-center rounded-r-2xl hover:bg-red hover:text-white text-black border-black cursor-pointer transition-colors duration-200 ease-in-out"
-    >
-      <Icon name="ph:sort-descending" />
-    </DropdownMenuTrigger>
-    <DropdownMenuPortal>
-      <DropdownMenuContent
-        class="flex flex-col border-2 border-black bg-white text-black rounded-2xl px-5 pt-4 pb-3 shadow-dialog will-change-[opacity,transform] gap-2 animate-slide-up"
-        :side-offset="5"
-        align="end"
+  <div>
+    <DropdownMenuRoot v-model:open="toggleState" :modal="false">
+      <DropdownMenuTrigger
+        class="flex-row px-5 border-2 h-10 items-center rounded-r-2xl hover:bg-red hover:text-white text-black border-black cursor-pointer transition-colors duration-200 ease-in-out hidden sm:flex"
       >
-        <template v-for="item in filteredNavigationItems">
-          <DropdownMenuSeparator
-            v-if="item.separator"
-            :class="item.class"
-            class="h-[1px] bg-black rounded-full"
+        <Icon name="ph:sort-descending" />
+      </DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent
+          class="flex flex-col border-2 border-black bg-white text-black rounded-2xl px-5 pt-4 pb-3 shadow-dialog will-change-[opacity,transform] gap-2 animate-slide-up"
+          :side-offset="5"
+          align="end"
+        >
+          <template v-for="item in filteredNavigationItems">
+            <DropdownMenuSeparator
+              v-if="item.separator"
+              :class="item.class"
+              class="h-[1px] bg-black rounded-full"
+            />
+            <DropdownMenuItem v-else :class="item.class">
+              <NuxtLink
+                :to="item.to"
+                class="flex flex-row items-center gap-2 hover:text-red"
+              >
+                <Icon :name="item.icon || 'ph:question'" />
+                <p :to="item.to" class="mt-1">
+                  {{ item.title }}
+                </p></NuxtLink
+              >
+            </DropdownMenuItem>
+          </template>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenuRoot>
+    <DrawerRoot>
+      <DrawerTrigger
+        class="flex flex-row px-5 border-2 h-10 items-center rounded-r-2xl hover:bg-red hover:text-white text-black border-black cursor-pointer transition-colors duration-200 ease-in-out w-fit sm:hidden"
+      >
+        <Icon name="ph:sort-descending" />
+      </DrawerTrigger>
+      <ClientOnly>
+        <DrawerPortal>
+          <DrawerOverlay
+            class="w-full h-full fixed top-0 left-0 bg-black/30 backdrop-blur-[2px]"
           />
-          <DropdownMenuItem v-else :class="item.class">
-            <NuxtLink
-              :to="item.to"
-              class="flex flex-row items-center gap-2 hover:text-red"
-            >
-              <Icon :name="item.icon || 'ph:question'" />
-              <p :to="item.to" class="mt-1">
-                {{ item.title }}
-              </p></NuxtLink
-            >
-          </DropdownMenuItem>
-        </template>
-      </DropdownMenuContent>
-    </DropdownMenuPortal>
-  </DropdownMenuRoot>
+          <DrawerContent
+            class="fixed bottom-0 left-0 w-full flex flex-col border-t-2 border-black bg-white text-black px-5 py-4 shadow-dialog will-change-[opacity,transform] gap-2 animate-slide-up h-min!"
+          >
+            <template v-for="item in filteredNavigationItems">
+              <DrawerItem v-if="!item.separator" :class="item.class">
+                <NuxtLink
+                  :to="item.to"
+                  class="flex flex-row items-center gap-2 hover:text-red"
+                >
+                  <Icon :name="item.icon || 'ph:question'" />
+                  <p :to="item.to" class="mt-1">
+                    {{ item.title }}
+                  </p>
+                </NuxtLink>
+              </DrawerItem>
+              <DrawerSeparator
+                v-else
+                :class="item.class"
+                class="h-[1px] bg-black rounded-full"
+              />
+            </template>
+          </DrawerContent>
+        </DrawerPortal>
+      </ClientOnly>
+    </DrawerRoot>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerRoot,
+  DrawerTrigger,
+} from "vaul-vue";
+
 const toggleState = ref(false);
 
 const userStore = {
   isAuthenticated: true, // Example authentication state
   fullName: "Branislav Juhás",
-  role: "user", // Example role, can be 'admin', 'user', etc.
+  role: "admin", // Example role, can be 'admin', 'user', etc.
   clubId: "12345", // Example club ID
 };
 
@@ -65,14 +111,14 @@ const navigationItems = [
   },
   {
     separator: true,
-    class: "flex sm:hidden",
+    class: "hidden sm:flex",
     filter: () => {
       return userStore.role && userStore.role !== "user";
     },
   },
   {
     separator: true,
-    class: "hidden sm:flex",
+    class: "sm:hidden",
   },
   {
     title: "Panel správy",
@@ -86,7 +132,7 @@ const navigationItems = [
     title: "Vyhlásenie GDPR",
     to: "/privacy",
     icon: "ph:fingerprint-simple",
-    class: "hidden sm:flex",
+    class: "sm:hidden",
   },
   {
     separator: true,
