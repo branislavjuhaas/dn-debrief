3ea423853cc052f0c81f57991cf3f9574169edb1
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-07 16:31:41 +0200

    feat: add wrapper component with title and hint props for improved layout

---
+++ dn-debrief/components/form/wrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col w-full">
+    <div class="flex flex-row justify-between items-center px-5">
+      <Label class="text-black font-bold">{{ props.title }}</Label>
+      <span v-if="props.hint" class="text-gray">{{ props.hint }}</span>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Label } from "reka-ui";
+
+const props = defineProps<{
+  title: string;
+  hint?: string;
+}>();
+</script>
+
+<style scoped></style>
