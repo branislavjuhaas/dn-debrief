213d798f587c09253d77d0ba387cdae136f9745b
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-07 09:56:07 +0200

    fix: add z-index to footer component for proper stacking context

--- dn-debrief/components/app/footer.vue
+++ dn-debrief/components/app/footer.vue
@@ -8,7 +8,7 @@ const userStore = {
 
 <template>
   <div
-    class="fixed bottom-0 w-full flex flex-col bg-white text-black min-h-[3.75rem] items-center justify-center px-5 border-t-2 border-black"
+    class="fixed bottom-0 w-full flex flex-col bg-white text-black min-h-[3.75rem] items-center justify-center px-5 border-t-2 border-black z-40"
   >
     <!-- <cookies-banner v-if="false" /> -->
     <div
