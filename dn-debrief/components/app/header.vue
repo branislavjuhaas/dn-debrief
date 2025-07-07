5abeb49c94eb2c6bc4495a94e78e8bc7528b86a0
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-07 09:56:36 +0200

    fix: update header component for improved structure and accessibility

--- dn-debrief/components/app/header.vue
+++ dn-debrief/components/app/header.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-import { NuxtImg } from "#components";
+import { NuxtImg, NuxtLink } from "#components";
 </script>
 
 <template>
   <header
-    class="fixed top-0 w-full bg-white text-black flex flex-col h-[3.75rem] items-center px-5 border-b-2 border-black"
+    class="fixed top-0 w-full bg-white text-black flex flex-col h-[3.75rem] items-center px-5 border-b-2 border-black z-40"
   >
     <div
-      class="grid grid-cols-[auto_1fr] sm:grid-cols-[1fr_auto_1fr] max-w-content h-full w-full gap-4 justify-center items-center"
+      class="grid grid-cols-[auto_1fr] sm:grid-cols-[1fr_32rem_1fr] max-w-content h-full w-full gap-4 justify-center items-center"
     >
-      <nuxt-link to="/" class="flex w-fit items-center">
-        <img src="@/assets/logo.svg" alt="DN DebRIEF II" class="h-8" />
-      </nuxt-link>
+      <NuxtLink
+        to="/"
+        class="flex w-fit items-center cursor-pointer"
+        aria-label="Go to homepage"
+      >
+        <img
+          src="@/assets/logo.svg"
+          alt="DN DebRIEF II logo"
+          class="h-8"
+          role="img"
+          aria-label="DN DebRIEF II logo"
+        />
+      </NuxtLink>
+      <AppSearchBar />
     </div>
   </header>
 </template>
