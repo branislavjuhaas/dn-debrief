7ab3056e16fbb09dcacbe4469bc984300559d255
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-08 21:48:29 +0200

    feat: add primary and disabled button variants in index page feat: create 404 page with a not found message refactor: update alert component layout for better structure

--- dn-debrief/components/app/alert.vue
+++ dn-debrief/components/app/alert.vue
@@ -39,12 +39,19 @@ function onOpenChange(open: boolean) {
       <AlertDialogContent
         class="z-[100] data-[state=open]:animate-content-show overflow-hidden fixed top-1/2 left-1/2 max-h-[85dvh] w-105 max-w-[calc(100dvw-2.5rem)] -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-white text-black border-2 border-black shadow-dialog focus:outline-none">
         <AlertDialogTitle
-          class="m-0 px-4 py-2 border-b-2 border-black text-base! font-bold flex items-center gap-2"
+          class="m-0 px-4 py-2 border-b-2 border-black text-base! font-bold flex items-center gap-2 justify-between"
           :class="
             alert.variant === 'critical' ? 'bg-red text-white' : 'bg-yellow'
           ">
-          <Icon :name="iconName" />
-          <span class="mt-1">{{ alert.title }}</span>
+          <div class="flex flex-row items-center gap-2">
+            <Icon :name="iconName" />
+            <span class="mt-1">{{ alert.title }}</span>
+          </div>
+          <AlertDialogCancel
+            aria-label="Close"
+            class="ease-in-out cursor-pointer">
+            <Icon name="ph:x" />
+          </AlertDialogCancel>
         </AlertDialogTitle>
         <div class="flex flex-col w-full p-4 gap-5">
           <AlertDialogDescription>
--- dn-debrief/pages/index.vue
+++ dn-debrief/pages/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <h1>Welcome to DebRIEF v2</h1>
+    <AppButton
+      variant="primary"
+      @click="() => console.log('Primary Button Clicked')">
+      Primary
+    </AppButton>
+    <AppButton variant="secondary" to="/auth">Secondary</AppButton>
+    <AppButton variant="tertiary" icon="ph:nut">Tertiary</AppButton>
+    <AppButton variant="ghost" external>Ghost</AppButton>
+    <AppButton variant="primary">With Icon</AppButton>
+    <AppButton variant="tertiary" icon="ph:nut" size="dialog">
+      Dialog Size
+    </AppButton>
+    <AppButton variant="primary" size="header">Header Size</AppButton>
+    <!--Disabled button variants-->
+    <AppButton variant="primary" disabled>Disabled Primary</AppButton>
+    <AppButton variant="secondary" disabled>Disabled Secondary</AppButton>
+    <AppButton variant="tertiary" disabled>Disabled Tertiary</AppButton>
+    <AppButton variant="ghost" disabled>Disabled Ghost</AppButton>
   </div>
 </template>
 
