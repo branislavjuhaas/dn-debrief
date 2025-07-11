bd36ea5edc0c25c92c9101e5870ccd30e54a3f70
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-11 11:28:50 +0200

    feat: add hoverable component with hover card functionality

---
+++ dn-debrief/components/app/hoverable.vue
@@ -0,0 +1,31 @@
+<template>
+  <HoverCardRoot v-model:open="hoverState">
+    <HoverCardTrigger class="flex outline-none w-fit h-fit">
+      <slot />
+    </HoverCardTrigger>
+    <HoverCardPortal>
+      <HoverCardContent
+        class="data-[side=bottom]:animate-slide-up data-[side=right]:animate-slide-left data-[side=left]:animate-slide-right data-[side=top]:animate-slide-down w-fit bg-white p-5 data-[state=open]:transition-all border-2 border-black shadow-dialog z-10 data-[side=top]:rounded-bl-lg data-[side=bottom]:rounded-tl-lg data-[side=left]:rounded-tr-lg data-[side=right]:rounded-tl-lg rounded-2xl translate-x-1/2"
+        side="bottom"
+        :side-offset="5"
+        align="center">
+        <div class="flex flex-col gap-2">
+          <slot name="content" />
+        </div>
+      </HoverCardContent>
+    </HoverCardPortal>
+  </HoverCardRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardPortal,
+  HoverCardRoot,
+  HoverCardTrigger,
+} from "reka-ui";
+import { ref } from "vue";
+
+const hoverState = ref(false);
+</script>
