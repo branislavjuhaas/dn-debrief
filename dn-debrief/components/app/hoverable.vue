c80f998514bf4a42cb2ea89b26412a694c2a653a
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-11 15:18:15 +0200

    fix: adjust padding and collision handling in HoverCardContent

--- dn-debrief/components/app/hoverable.vue
+++ dn-debrief/components/app/hoverable.vue
@@ -5,9 +5,9 @@
     </HoverCardTrigger>
     <HoverCardPortal>
       <HoverCardContent
-        class="data-[side=bottom]:animate-slide-up data-[side=right]:animate-slide-left data-[side=left]:animate-slide-right data-[side=top]:animate-slide-down w-fit bg-white p-5 data-[state=open]:transition-all border-2 border-black shadow-dialog z-10 data-[side=top]:rounded-bl-lg data-[side=bottom]:rounded-tl-lg data-[side=left]:rounded-tr-lg data-[side=right]:rounded-tl-lg rounded-2xl translate-x-1/2"
-        side="bottom"
+        class="data-[side=bottom]:animate-slide-up data-[side=right]:animate-slide-left data-[side=left]:animate-slide-right data-[side=top]:animate-slide-down w-fit bg-white px-5 py-3 data-[state=open]:transition-all border-2 border-black shadow-dialog z-10 data-[side=top]:rounded-bl-lg data-[side=bottom]:rounded-tl-lg data-[side=left]:rounded-tr-lg data-[side=right]:rounded-tl-lg rounded-2xl translate-x-1/2"
         :side-offset="5"
+        :collision-padding="{ bottom: 60 }"
         align="center">
         <div class="flex flex-col gap-2">
           <slot name="content" />
