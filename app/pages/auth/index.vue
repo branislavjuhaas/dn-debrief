67081c0ddd9ee50826781ca7b7df47a7d86ea293
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-06 21:48:45 +0100

    feat: add login dialog with Google and GitHub authentication options

---
+++ app/pages/auth/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import AppDialog from "~/components/dialog/AppDialog.vue";
+
+const loginGoogle = () => {};
+const loginGithub = () => {};
+</script>
+
+<template>
+  <UPageSection>
+    <ProseH1>Prihlásenie na platformu DN Cascade</ProseH1>
+    <AppDialog>
+      <DialogHeader title="Vitajte späť!">
+        Nemáte ešte účet?
+        <NuxtLink to="/auth/register" class="text-secondary font-medium">
+          Zaregistrujte sa!
+        </NuxtLink>
+      </DialogHeader>
+      <div class="flex flex-row gap-3 w-full">
+        <UButton
+          variant="subtle"
+          color="neutral"
+          icon="ph:google-logo"
+          block
+          class="cursor-pointer">
+          Google
+        </UButton>
+        <UButton
+          variant="subtle"
+          color="neutral"
+          icon="ph:github-logo"
+          block
+          class="cursor-pointer">
+          GitHub
+        </UButton>
+      </div>
+    </AppDialog>
+  </UPageSection>
+</template>
+
+<style scoped></style>
