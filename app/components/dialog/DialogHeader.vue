82d6cc75012fc8670bf0149b6b251f0f1bc33876
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-06 19:19:51 +0100

    feat: add DialogHeader component for structured UI presentation

---
+++ app/components/dialog/DialogHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { any } from "better-auth";
+
+const props = defineProps<{
+  icon?: string;
+  title: string;
+  description?: string;
+}>();
+
+const slots = defineSlots<{
+  default?: () => any;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center max-w-92">
+    <UIcon
+      v-if="icon"
+      :name="props.icon || ''"
+      class="mb-4 sm:mb-6 h-16 w-16" />
+    <h2 class="text-xl font-semibold text-center leading-7 text-highlighted">
+      {{ title }}
+    </h2>
+    <p v-if="description || !!slots.default" class="text-center text-muted">
+      <slot>
+        {{ description }}
+      </slot>
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
