6e32a2359caec484adf225ebf56e0464b1fffe18
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-06 19:20:02 +0100

    feat: add navigation dropdown menu with user profile and logout functionality

--- app/components/header/AppNav.vue
+++ app/components/header/AppNav.vue
@@ -1,5 +1,51 @@
 <script setup lang="ts">
+import { createAuthClient } from "better-auth/vue";
+
+const authClient = createAuthClient();
 const userStore = useUserStore();
+
+const logout = async () => {
+  authClient.signOut();
+  userStore.clear();
+};
+
+const navItems = ref([
+  [
+    {
+      label: "Môj profil",
+      icon: "ph:identification-card",
+      to: "/profile",
+    },
+    {
+      label: "Moje platby",
+      icon: "ph:coins",
+      disabled: true,
+    },
+    {
+      label: "Moje registrácie",
+      icon: "ph:books",
+      disabled: true,
+    },
+    {
+      label: "Moje výsledky",
+      icon: "ph:scroll",
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: "Panel správy",
+      icon: "ph:nut",
+    },
+  ],
+  [
+    {
+      label: "Odhlásiť sa",
+      icon: "ph:plugs",
+      onSelect: () => logout(),
+    },
+  ],
+]);
 </script>
 
 <template>
@@ -19,6 +65,15 @@ const userStore = useUserStore();
       }">
       {{ userStore.user?.name }}
     </UButton>
+    <UDropdownMenu
+      :items="navItems"
+      :content="{
+        align: 'end',
+        side: 'bottom',
+        sideOffset: 8,
+      }">
+      <UButton icon="ph:sort-ascending" color="neutral" variant="outline" />
+    </UDropdownMenu>
   </UFieldGroup>
   <UButton
     v-else
