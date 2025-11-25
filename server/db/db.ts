f0e383966f9dc88b336e8fffb8b1a5d90fb5165f
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-25 18:19:10 +0100

    feat: integrate authentication, clubs, and events schemas into database configuration

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,4 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
+import * as authSchema from "./schema/auth";
+import * as clubsSchema from "./schema/clubs";
+import * as eventsSchema from "./schema/events";
 
-export const db = drizzle(process.env.DATABASE_URL as string);
+export const db = drizzle(process.env.DATABASE_URL as string, {
+  schema: { ...authSchema, ...clubsSchema, ...eventsSchema },
+  mode: "default",
+});
