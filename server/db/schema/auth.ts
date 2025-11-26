cb512f018e4ef2bd16e96d75e814d0b494fcc09e
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-26 08:45:01 +0100

    feat: update verifications table to use serial type for id column

--- server/db/schema/auth.ts
+++ server/db/schema/auth.ts
@@ -162,7 +162,7 @@ export const accountRelations = relations(accounts, ({ one }) => ({
 export const verifications = pgTable(
   "verifications",
   {
-    id: text("id").primaryKey(),
+    id: serial("id").primaryKey(),
     identifier: text("identifier").notNull(),
     value: text("value").notNull(),
     expiresAt: timestamp("expires_at").notNull(),
