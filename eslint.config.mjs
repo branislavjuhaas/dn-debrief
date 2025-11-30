b70b135da8d487d8137d4e44bb25ba702fb0a1b9
Branislav Juhás <juhas.branislaw@gmail.com>
2025-11-30 13:31:21 +0100

    chore: Update ESLint configuration and bump a better-auth version to 1.4.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt()
-  .prepend()
-  .override("nuxt/typescript", {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
-  });
+export default withNuxt({
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+});
