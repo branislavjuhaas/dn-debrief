e5d3c2752519774832d99496896d657e5562cff0
Branislav Juhás <juhas.branislaw@gmail.com>
2025-12-01 20:33:57 +0100

    chore: Add impersonation banner to user interface for enhanced user experience

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
   <UApp :locale="sk" :ui="{ colorScheme: 'light' }">
     <NuxtRouteAnnouncer />
+    <UBanner v-if="userStore.impersonation" icon="ph:visor-bold" color="info">
+      <template #title>
+        <p>
+          <b>Ja je niekto iný</b>
+          | Dočasne zosobňujete používateľa {{ userStore.fullName }}.
+        </p>
+      </template>
+    </UBanner>
     <AppHeader />
     <NuxtLoadingIndicator color="var(--ui-primary)" />
     <UMain>
@@ -16,4 +24,6 @@ import { sk } from "@nuxt/ui/locale";
 
 import AppHeader from "~/components/header/AppHeader.vue";
 import AppFooter from "~/components/footer/AppFooter.vue";
+
+const userStore = useUserStore();
 </script>
