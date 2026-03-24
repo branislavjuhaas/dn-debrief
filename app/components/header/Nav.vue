b62080fbce565fefd2af5f4fefb559af549324f2
Branislav Juhás <juhas.branislaw@gmail.com>
2026-03-24 21:25:44 +0100

    feat: add navigation component with user profile and logout functionality

---
+++ app/components/header/Nav.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+const userStore = useUserStore();
+const authClient = useAuthClient();
+
+const logout = async () => {
+  authClient.signOut();
+  userStore.clear();
+};
+
+const navItems = ref([
+  [
+    {
+      label: "Môj profil",
+      icon: "ph:identification-card",
+      to: "/profile",
+    },
+    {
+      label: "Moje platby",
+      icon: "ph:coins",
+      disabled: true,
+    },
+    {
+      label: "Moje registrácie",
+      icon: "ph:books",
+      disabled: true,
+    },
+    {
+      label: "Moje výsledky",
+      icon: "ph:scroll",
+      disabled: true,
+    },
+  ],
+  ...(userStore.isAuthenticated &&
+  (userStore.user.role !== "user" || userStore.user.managedClubs.length > 0)
+    ? [
+        [
+          {
+            label: "Panel správy",
+            to: "/manage",
+            icon: "ph:nut",
+          },
+        ],
+      ]
+    : []),
+  [
+    {
+      label: "Odhlásiť sa",
+      icon: "ph:plugs",
+      onSelect: () => logout(),
+    },
+  ],
+]);
+</script>
+
+<template>
+  <template v-if="userStore.isAuthenticated">
+    <UFieldGroup>
+      <UButton
+        to="/profile"
+        color="neutral"
+        variant="outline"
+        :avatar="{
+          src: `${userStore.user?.image}`,
+          alt: `${userStore.fullName}`,
+          chip: userStore.impersonation
+            ? { color: 'info', inset: true }
+            : false,
+        }"
+      >
+        {{ userStore.fullName }}
+      </UButton>
+      <UDropdownMenu
+        :items="navItems"
+        :content="{
+          align: 'end',
+          side: 'bottom',
+          sideOffset: 8,
+        }"
+      >
+        <UButton icon="ph:sort-ascending" color="neutral" variant="outline" />
+      </UDropdownMenu>
+    </UFieldGroup>
+  </template>
+  <UButton v-else to="/auth" color="neutral" variant="subtle" icon="ph:keyhole">
+    Prihlásenie
+  </UButton>
+</template>
+
+<style scoped></style>
