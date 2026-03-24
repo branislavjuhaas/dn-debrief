92f582f45a10a68c0aaa97d2a2bc62171befb4a8
Branislav Juhás <juhas.branislaw@gmail.com>
2026-03-24 21:25:57 +0100

    feat: add search button to header with authentication check

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const userStore = useUserStore();
+</script>
 
 <template>
   <UHeader>
     <template #left>
       <DebriefLogo />
+    </template>
+    <UDashboardSearchButton
+      class="w-96"
+      :disabled="!userStore.isAuthenticated"
+      icon="ph:magnifying-glass"
+      @click="searchOpen = true"
+    >
+      {{
+        userStore.isAuthenticated
+          ? "Hľadať ľudí, podujatia a kluby..."
+          : "Pre vyhľadávanie sa prihláste"
+      }}
+    </UDashboardSearchButton>
+    <template #right>
+      <UDashboardSearchButton
+        :disabled="!userStore.isAuthenticated"
+        class="lg:hidden"
+        collapsed
+        icon="ph:magnifying-glass"
+      />
+      <HeaderNav />
     </template>
   </UHeader>
 </template>
