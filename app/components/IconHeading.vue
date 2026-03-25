5471f6f33b9b099c1a08ccfd6b6e2b828a9757b6
Branislav Juhás <juhas.branislaw@gmail.com>
2026-03-25 11:03:36 +0100

    feat: add IconHeading component with icon and slot support

---
+++ app/components/IconHeading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row gap-1 items-center">
+    <UIcon :name="icon" class="size-5" />
+    <h3 class="text-base font-bold">
+      <slot />
+    </h3>
+  </div>
+</template>
+
+<style scoped></style>
