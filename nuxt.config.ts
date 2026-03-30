08997ff5054ee98b64f29390fb04aedda69e72bd
Branislav Juhás <juhas.branislaw@gmail.com>
2026-03-30 18:48:33 +0200

    feat: enable experimental tasks in Nitro configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,10 @@ export default defineNuxtConfig({
 
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-07-15",
+
+  nitro: {
+    experimental: {
+      tasks: true,
+    },
+  },
 });
