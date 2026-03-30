f611a94c1ad644e1847ab535e20ea7dc7003b889
Branislav Juhás <juhas.branislaw@gmail.com>
2026-03-30 18:48:07 +0200

    feat: refactor Card component to use Event object for props

--- app/components/event/Card.vue
+++ app/components/event/Card.vue
@@ -1,38 +1,22 @@
 <script setup lang="ts">
 import formatRange from "#shared/utils/format-range";
+import type { Event } from "#shared/types/event";
 
 const props = defineProps<{
-  name: string;
-  slug: string;
-  thumbnail: string;
-  place: string;
-  beginning: Date;
-  end: Date;
-  badges?: {
-    text: string;
-    color:
-      | "primary"
-      | "success"
-      | "secondary"
-      | "info"
-      | "warning"
-      | "error"
-      | "neutral";
-    variant?: "subtle" | "outline";
-  }[];
+  event: Event;
 }>();
 </script>
 
 <template>
   <NuxtLink
-    :to="`/events/${props.slug}`"
+    :to="`/events/${props.event.slug}`"
     class="min-w-[calc(33.33%-0.66rem)] w-96 shrink-0 flex">
     <UCard
       class="event-card w-full shadow hover:bg-muted"
       :ui="{ body: 'sm:p-0' }">
       <div class="aspect-[7/3] w-full overflow-hidden">
         <NuxtImg
-          :src="props.thumbnail"
+          :src="props.event.thumbnailUrl || '/images/event-placeholder.png'"
           alt="Event thumbnail"
           class="object-cover aspect-[7/3] w-full transition-transform duration-300" />
       </div>
@@ -40,21 +24,23 @@ const props = defineProps<{
       <template #footer>
         <div class="flex flex-row items-start justify-between">
           <span class="text-sm font-semibold mt-0.5">
-            {{ props.name }}
+            {{ props.event.name }}
           </span>
           <div class="flex flex-row gap-1">
-            <UBadge
-              v-for="badge in props.badges"
-              :key="badge.text"
-              :color="badge.color"
-              :variant="badge.variant ?? 'subtle'"
-              class="text-nowrap">
-              {{ badge.text }}
-            </UBadge>
+            <!-- TODO: Add badge assignment algorithm -->
+            <!-- <UBadge -->
+            <!--   v-for="badge in props.badges" -->
+            <!--   :key="badge.text" -->
+            <!--   :color="badge.color" -->
+            <!--   :variant="badge.variant ?? 'subtle'" -->
+            <!--   class="text-nowrap"> -->
+            <!--   {{ badge.text }} -->
+            <!-- </UBadge> -->
           </div>
         </div>
         <span class="text-sm text-muted">
-          {{ formatRange(beginning, end) }} - {{ props.place }}
+          {{ formatRange(props.event.beginning, props.event.end) }} -
+          {{ props.event.place }}
         </span>
       </template>
     </UCard>
