8c6ed2bfa6f7dce5a41c2378b64026cde2f39dac
Branislav Juhás <juhas.branislaw@gmail.com>
2026-06-01 22:21:53 +0200

    fix: update drizzle driver to reflect configuration updates in drizzle 1.0.0-rc.x

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,13 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import * as auth from "./schema/auth";
-import * as clubs from "./schema/clubs";
-import * as payments from "./schema/payments";
-import * as events from "./schema/events";
-import * as settings from "./schema/settings";
-
 import { relations } from "./schema/relations";
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-  schema: { ...auth, ...clubs, ...payments, ...events, ...settings },
   relations,
 });
