184818c892645e08ff00e6bb0f23a0b5a79b852a
Branislav Juhás <juhas.branislaw@gmail.com>
2026-06-03 18:46:24 +0200

    fix: edit type of club to prevent type errors

--- shared/types/club.d.ts
+++ shared/types/club.d.ts
@@ -3,5 +3,5 @@ import type { clubMemberships, clubs } from "#server/db/schema/clubs";
 export type Club = typeof clubs.$inferSelect;
 
 export type ClubMembership = typeof clubMemberships.$inferSelect & {
-  club: Club;
+  club?: Club | null;
 };
