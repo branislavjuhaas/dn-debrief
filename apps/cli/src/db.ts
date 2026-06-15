d2ab6bd244976af1e6901f0e814ee44383ab58ab
Branislav Juhás <98485942+branislavjuhaas@users.noreply.github.com>
2026-06-15 19:16:11 +0000

    feat: populate database utilities in cli

--- apps/cli/src/db.ts
+++ apps/cli/src/db.ts
@@ -1,4 +1,9 @@
+import { cliDb } from "@dn-debrief/db/cli";
 import { defineCommand } from "citty";
+import consola from "consola";
+import { readUserConfig } from "rc9";
+
+let database = null;
 
 const seed = defineCommand({
   meta: { name: "seed", description: "Seed the database with mock data" },
@@ -8,4 +13,20 @@ const seed = defineCommand({
 export const db = defineCommand({
   meta: { name: "db", description: "Database management commands" },
   subCommands: { seed },
+  async setup() {
+    const config = readUserConfig(".debriefrc");
+
+    if (!config.database) {
+      consola.error("Missing database URL. Configure before using database!");
+    }
+
+    try {
+      database = cliDb(config.database);
+    } catch {
+      consola.error("Could not connect to the database at specified url!");
+      return;
+    }
+
+    consola.info(`Connected to database: ${database ? "Success" : "Failed"}`);
+  },
 });
