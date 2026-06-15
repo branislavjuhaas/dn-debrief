aae20ba6d73c9ee21ab9bc65291afd94274a9f55
Branislav Juhás <98485942+branislavjuhaas@users.noreply.github.com>
2026-06-15 14:07:16 +0000

    feat: make init command interactive

--- apps/cli/src/init.ts
+++ apps/cli/src/init.ts
@@ -1,22 +1,25 @@
 import { defineCommand } from "citty";
 import consola from "consola";
+import { colorize } from "consola/utils";
 import { writeUserConfig } from "rc9";
 
 export const init = defineCommand({
   meta: { name: "init", description: "Initialize instance of the cli" },
-  args: {
-    database: {
-      type: "string",
-      description: "Database URL of the system's database",
-    },
-  },
-  run({ args }) {
+  async run() {
+    consola.log(
+      `${colorize("blueBright", "Welcome to the Intelligent Debate platform—welcome to the 21st century!")}\nTo configure the CLI, enter following parameters`,
+    );
+
+    const database = await consola.prompt(
+      "Database URL of the system's database:",
+    );
+
     const config = {
-      database: args.database,
+      database,
     };
 
     writeUserConfig(config, ".debriefrc");
 
-    consola.success("Written working configuration for the user");
+    consola.success("Working configuration for the user written");
   },
 });
