709ad5d24b6cb0f89b045feaadca8a290d7853cb
Branislav Juhás <juhas.branislaw@gmail.com>
2026-08-11 19:56:32 +0200

    feat: add nuxt ui to the project

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,6 +5,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  modules: ["@nuxt/ui"],
+
   nitro: {
     experimental: {
       openAPI: true,
