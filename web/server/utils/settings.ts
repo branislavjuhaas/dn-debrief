549d43a56eae88ed18f5a5948f592bc37e9001d9
Branislav Juhás <juhas.branislaw@gmail.com>
2026-08-11 09:39:23 +0200

    feat(api): add option to set setting in settings helper util

--- web/server/utils/settings.ts
+++ web/server/utils/settings.ts
@@ -1,4 +1,5 @@
 import { db } from "#server/db";
+import { settings } from "#server/db/schema/settings";
 
 export const getSetting = async (key: "current-seasons") => {
   const settings = await db.query.settings.findFirst();
@@ -12,4 +13,24 @@ export const getSetting = async (key: "current-seasons") => {
     return filteredSeasons;
   }
   return null;
+};
+
+export const setSetting = async (key: "current-seasons", value: number[]) => {
+  // If there is no settings record, create one
+  const existingSettings = await db.query.settings.findFirst();
+  if (!existingSettings) {
+    const currentSeasons =
+      key === "current-seasons" ? value : [new Date().getFullYear()];
+
+    await db.insert(settings).values({ currentSeasons: currentSeasons });
+    return null;
+  }
+
+  if (key === "current-seasons") {
+    const result = await db
+      .update(settings)
+      .set({ currentSeasons: value })
+      .returning();
+    return result[0];
+  }
 };
