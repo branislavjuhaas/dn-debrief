a1c1a472bdb6c481bf52c844cd81b69d1aaf4fb7
Branislav Juhás <juhas.branislaw@gmail.com>
2026-09-20 12:47:39 +0200

    feat(web): create primitive registration config section

--- web/app/components/event/RegistrationConfig.vue
+++ web/app/components/event/RegistrationConfig.vue
@@ -1,9 +1,73 @@
 <script setup lang="ts">
+import { z } from "zod";
+import {
+  CalendarDateTime,
+  parseAbsoluteToLocal,
+  toCalendarDateTime,
+} from "@internationalized/date";
+
 import type { Event } from "#shared/types/event";
 
 const model = defineModel<Partial<Event>>({
   default: () => ({}),
 });
+
+const collectRegistrations = ref(false);
+
+const externalForm = useTemplateRef("externalForm");
+
+const validate = async () => {
+  if (!externalForm.value) return false;
+
+  const externalResult = await externalForm.value.validate({ silent: true });
+  return externalResult;
+};
+
+defineExpose({
+  validate,
+});
+
+const externalConfigSchema = z.object({
+  deadline: z
+    .any()
+    .refine(
+      (value) => value instanceof CalendarDateTime,
+      "Dátum je povinný údaj",
+    ),
+  href: z.url("Link na registračný formulár musí byť platný URL"),
+});
+
+// Writable computed property for Deadline
+const deadline = computed<CalendarDateTime>({
+  get: () => {
+    const raw = model.value.registrationConfig?.deadline;
+    const isoString = raw ?? new Date().toISOString();
+    return toCalendarDateTime(parseAbsoluteToLocal(isoString));
+  },
+  set: (val: CalendarDateTime) => {
+    model.value.registrationConfig = {
+      ...model.value.registrationConfig,
+      deadline: val.toString(), // converts CalendarDateTime to ISO string
+    };
+  },
+});
+
+// Writable computed property for Href
+const href = computed<string>({
+  get: () => model.value.registrationConfig?.href ?? "",
+  set: (val: string) => {
+    model.value.registrationConfig = {
+      ...model.value.registrationConfig,
+      href: val,
+    };
+  },
+});
+
+// Combined object for UForm validation state
+const formState = computed(() => ({
+  deadline: deadline.value,
+  href: href.value,
+}));
 </script>
 
 <template>
@@ -13,22 +77,39 @@ const model = defineModel<Partial<Event>>({
       description="Platforma DebRIEF teraz ponúka možnosť registrácie na podujatia priamo v systéme s možnosťou platieb za registráciu, úpravy údajov a ich exportu.">
       <template #title>
         Zbierať registrácie priamo v DebRIEF-e?
-        <!-- <UBadge size="lg" variant="subtle" color="warning" class="-mt-3 ml-2">
-          Experimentálne
-        </UBadge> -->
         <UBadge size="lg" variant="subtle" color="error" class="-mt-3 ml-2">
           Neimplementované
         </UBadge>
       </template>
       <template #footer>
-        <USwitch label="Centrálny zber registrácií" disabled />
+        <USwitch
+          v-model="collectRegistrations"
+          label="Centrálny zber registrácií"
+          disabled />
       </template>
       <div
-        class="flex h-full w-full lg:min-h-36 overflow-hidden relative rounded-lg max-lg:hidden opacity-70">
+        class="flex h-full w-full lg:min-h-36 overflow-hidden relative rounded-lg max-lg:hidden opacity-60">
         <NuxtImg
           src="/assets/registrations-banner.jpg"
-          class="absolute w-full ml-auto object-cover object-top grayscale" />
+          class="absolute w-full ml-auto object-cover object-top" />
       </div>
     </UPageCard>
+
+    <UForm
+      ref="externalForm"
+      :schema="externalConfigSchema"
+      :state="formState"
+      class="space-y-4">
+      <UFormField name="href" label="Registračný formulár">
+        <UInput
+          v-model="href"
+          type="url"
+          placeholder="https://forms.ju.dev/YgmJX2MSeJqw2w8K9" />
+      </UFormField>
+
+      <UFormField name="deadline" label="Deadline">
+        <UInputDate v-model="deadline" granularity="minute" class="w-full" />
+      </UFormField>
+    </UForm>
   </div>
 </template>
--- web/app/pages/manage/events/new.vue
+++ web/app/pages/manage/events/new.vue
@@ -2,8 +2,13 @@
 import type { Event } from "#shared/types/event";
 import type { TabsItem } from "@nuxt/ui";
 import z from "zod";
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const mdAndLarger = breakpoints.greaterOrEqual("md");
 
 const detailsEditor = useTemplateRef("detailsEditor");
+const registrationConfig = useTemplateRef("registrationConfig");
 
 const { data: userData } = await useFetch("/api/users/me", {
   key: "users-me",
@@ -37,7 +42,7 @@ const newEvent = ref<Partial<Event>>({
     ],
   },
   registrationConfig: {
-    deadline: undefined,
+    deadline: new Date().toISOString(),
     href: undefined,
   },
   organizers: [
@@ -47,8 +52,19 @@ const newEvent = ref<Partial<Event>>({
   ],
 });
 
+const toast = useToast();
+
 const createEvent = async () => {
-  if (!(await detailsEditor.value!.validate())) {
+  if (
+    !(await detailsEditor.value!.validate()) ||
+    !(await registrationConfig.value!.validate())
+  ) {
+    toast.add({
+      title: "Chýbajúce údaje",
+      description:
+        "Pred vytvorením podujatia musíte vyplniť všetky potrebné údaje.",
+      color: "error",
+    });
     return;
   }
 };
@@ -79,28 +95,39 @@ const activeTab = ref("basic");
             <UDashboardSidebarCollapse />
           </template>
         </UDashboardNavbar>
-        <UDashboardToolbar>
-          <UTabs v-model="activeTab" :items="items" :content="false" />
-          <UButton
-            label="Vytvoriť podujatie"
-            color="primary"
-            variant="solid"
-            icon="i-ph-checks"
-            @click="createEvent" />
-        </UDashboardToolbar>
+        <ClientOnly>
+          <UDashboardToolbar class="flex flex-col md:flex-row max-md:py-2">
+            <UTabs
+              v-model="activeTab"
+              :items="items"
+              :content="false"
+              :orientation="mdAndLarger ? 'horizontal' : 'vertical'"
+              :ui="{ list: 'max-md:w-full' }"
+              class="max-md:w-full" />
+            <UButton
+              label="Vytvoriť podujatie"
+              color="primary"
+              variant="solid"
+              icon="i-ph-checks"
+              @click="createEvent"
+              :block="!mdAndLarger" />
+          </UDashboardToolbar>
+        </ClientOnly>
       </template>
       <template #body>
-        <div v-if="activeTab === 'basic'">
+        <div v-show="activeTab === 'basic'">
           <UPageHeader title="Základné informácie" class="mb-4" />
           <EventDetailsEditor ref="detailsEditor" v-model="newEvent" />
         </div>
-        <div v-else-if="activeTab === 'organizers'">
+        <div v-show="activeTab === 'organizers'">
           <UPageHeader title="Organizátori/-ky" class="mb-4" />
           <EventOrganizers v-model="newEvent" />
         </div>
-        <div v-else-if="activeTab === 'registration-details'">
+        <div v-show="activeTab === 'registration-details'">
           <UPageHeader title="Detaily registrácie" class="mb-4" />
-          <EventRegistrationConfig v-model="newEvent" />
+          <EventRegistrationConfig
+            ref="registrationConfig"
+            v-model="newEvent" />
         </div>
       </template>
     </UDashboardPanel>
