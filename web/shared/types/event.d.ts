6d331e5f7249cc6ff81fb7f2a9dd9d0679010d5d
Branislav Juhás <juhas.branislaw@gmail.com>
2026-09-23 17:37:59 +0200

    feat(api): add organizers' image to the event

--- web/shared/types/event.d.ts
+++ web/shared/types/event.d.ts
@@ -1,9 +1,18 @@
 import type { events } from "#server/db/schema/events";
+import type { UserRole } from "./user";
 
 export type Event = Omit<
   SerializeInferredDates<typeof events.$inferSelect>,
   "address"
 > & {
   address: string;
-  organizers: Array<{ id: number }>;
+  organizers: Array<{
+    id: number;
+    name: string;
+    surname: string;
+    role: UserRole;
+    email: string;
+    phone: string;
+    image: string | null;
+  }>;
 };
